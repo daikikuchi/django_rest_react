ded710b42b391a347c4758d1355af0655c66aef0
dai <kikuchi.dai@gmail.com>
2020-06-12 15:29:19 +0900

    Display a list of leads.

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,17 +1,31 @@
 import axios from 'axios';
 
-import { GET_LEADS } from './types';
+import { GET_LEADS, DELETE_LEAD } from './types';
 // any action we want to use is here
 // dispatch an action to reducer
 
 // GET LEADS
 export const getLeads = () => dispatch => {
-  axios.get('/api/leads')
-    .then(res => {
+  axios.get('/api/leads/')
+    .then((res) => {
       // disptch GET_LEADS action to reducer
       dispatch({
         type: GET_LEADS,
         payload: res.data
       });
-    }).catch(err => console.log(err));
+    }).catch((err) => console.log(err));
 }
+
+// Delete Lead
+
+export const deleteLeads = (id) => disptch => {
+  axios
+    .get(`/apileads/${id}`)
+    .then(res => {
+      disptch({
+        type: DELETE_LEAD,
+        payload: id
+      });
+    })
+    .catch(err => console.lof(err))
+  }
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getLeads } from '../../actions/leads';
@@ -7,13 +7,42 @@ import { getLeads } from '../../actions/leads';
 export class Leads extends Component {
   static propTypes = {
     leads: PropTypes.array.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
   }
 
   render() {
     return (
-      <div>
-        <h1>Leads list</h1>
-      </div>
+      <>
+         <h2>Leads</h2>
+         <table className="table table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            { this.props.leads.map(lead => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                 <button className="btn btn-danger btn">
+                 Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+         </table>
+      </>
     );
   }
 }
@@ -23,4 +52,4 @@ const mapStateToProps = state => ({
 });
 
 // whenever we use connect we need to export default connect
-export default connect(mapStateToProps)(Leads);
+export default connect(mapStateToProps, { getLeads })(Leads);
