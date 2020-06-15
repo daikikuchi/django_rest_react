6aa67bab4f2a05fdca2678a424a350675315c5c6
dai <kikuchi.dai@gmail.com>
2020-06-15 13:27:42 +0900

    Added reducer and action for messages

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { createMessage } from './messages';
 
-import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from './types';
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from './types';
 // any action we want to use is here
 // dispatch an action to reducer
 
@@ -22,6 +23,7 @@ export const deleteLead = (id) => disptch => {
   axios
     .delete(`/api/leads/${id}`)
     .then(res => {
+      disptch(createMessage({deleteLead : 'Lead Deleted'}))
       disptch({
         type: DELETE_LEAD,
         payload: id
@@ -35,10 +37,20 @@ export const deleteLead = (id) => disptch => {
     axios
       .post('/api/leads/', lead)
       .then(res => {
+        dispatch(createMessage({ addLead : 'Lead Added'}))
         dispatch({
             type: ADD_LEAD,
             payload: res.data
         })
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        const errors = {
+            msg: err.response.data,
+            status: err.response.status,
+        };
+        dispatch({
+          type: GET_ERRORS,
+          payload: errors,
+        });
+      });
    }
